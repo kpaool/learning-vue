ac75507658357fa1f898255229e9a975ae4b4936
kpaool <jonathanpkatumba@gmail.com>
2026-03-03 09:17:08 +0300

    feat: pinia

---
+++ src/stores/userStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { ref,reactive } from "vue";
+
+export const useUserStore = defineStore("user",()=>{
+
+    const user = reactive({
+        username:"",
+        role:"",
+        token:""
+    })
+
+    function setUser(username,role,token){
+        user.username = username
+        user.role = role
+        user.token = token
+    }
+
+    return { user,setUser }
+})
